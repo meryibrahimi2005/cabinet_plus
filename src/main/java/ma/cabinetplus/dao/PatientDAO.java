d906843097f603d808285d6c39c534dc0b6e0cee
Meryem Ibrahimi <meryibrahimi2005@gmail.com>
2025-11-21 17:22:39 +0100

    mm

--- src/main/java/ma/cabinetplus/dao/PatientDAO.java
+++ src/main/java/ma/cabinetplus/dao/PatientDAO.java
@@ -5,9 +5,4 @@ import ma.cabinetplus.model.Patient;
 public interface PatientDAO extends GenericDAO<Patient, Integer> {
     Patient trouverParNumeroDossier(String numeroDossier);
     Patient trouverParUsername(String username);
-<<<<<<< HEAD
-=======
-    List<Patient> trouverTous();
-    Patient trouverParNumeroDossier(String numeroDossier);
->>>>>>> 0aeb5b6 (couche service)
 }
